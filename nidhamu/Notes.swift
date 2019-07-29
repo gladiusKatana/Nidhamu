528b7d38b659fb6f01eca4d627dc1c27a457fe5f
gladiusKatana <garthsnyder@protonmail.com>
2019-07-29 13:35:46 -0400

    Updated Notes.swift with new feature roadmap

--- nidhamu/Notes.swift
+++ nidhamu/Notes.swift
@@ -6,25 +6,32 @@
  todo                                           deadline                                            //(todos organized by deadline & ~kind)
  
                                                                                                     //data model
- ⁌o⁍ classifierVC no longer first responder after landscape/update date
- timetableVC deferral-date selection mode
- cascading deferrals
- mark item as recurring
-                                                July 29
+ ⁌o⁍ classifierVC no longer first responder (although displayed) after a test condition: switching to the Settings app / advancing device date, then
+                                                                                         resuming the app, *twice* (bug occurs on >= 2nd time) all while
+                                                                                         keeping this app in landscape (bug nonexistent in portrait) *
+ 
+                  * this bug seems to be layout-related. Will attempt to obviate it by extending app's orientation adaptibility
+                  (eg no autorotating after transitioning from a differently-oriented other app)
+ 
+ timetableVC deferral-date selection mode       after bug is fixed
+ cascading deferrals                            soon after above
+ mark item as recurring                         soon after above
+ 
+                                                Aug 1
  
  archive data (eventsAtDate & AtIndexPath)
  CSV emails of archive data
-                                                Aug 1
+                                                Aug 3
  
                                                                                                     //views
  calendarVC; populate with eventsAtDate
  morn/aft/eve/night VC
  list VC (today items)
  list VC (deadlineless items)
-                                                Aug 3
+                                                Aug 5
  
                                                                                                     //UI
- optimize navigation
+ optimize navigation (being handled with the above bug)
  settings
  buttons
  launchScreen
