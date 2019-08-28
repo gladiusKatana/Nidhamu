41113655b37428f8887d0e756a44f4468a1b410c
gladiusKatana <garthsnyder@protonmail.com>
2019-08-28 17:15:50 -0400

    Reorg & renaming

--- nidhamu/Core/AppDelegate.swift
+++ nidhamu/Core/AppDelegate.swift
@@ -50,21 +50,5 @@ import UIKit
     }
 }
 
-struct AppUtility {
-    
-    static func lockOrientation(_ orientation: UIInterfaceOrientationMask) {
-        if let appDelegate = UIApplication.shared.delegate as? AppDelegate {
-            appDelegate.orientationLock = orientation
-        }
-        else {
-            print("[AppUtility] error casting app delegate instance")
-        }
-    }
-    
-    static func lockOrientation(_ orientation: UIInterfaceOrientationMask, andRotateTo rotateOrientation:UIInterfaceOrientation) {
-        self.lockOrientation(orientation)
-        UIDevice.current.setValue(rotateOrientation.rawValue, forKey: "orientation")
-    }
-    
-}
+
 
---
+++ nidhamu/V/RotateLock.swift
@@ -0,0 +1,20 @@
+// RotateLock       ･   nidhamu   ･     created by Garth Snyder   aka   gladiusKatana  ⚔️
+import UIKit
+
+struct AppUtility {
+    
+    static func lockOrientation(_ orientation: UIInterfaceOrientationMask) {
+        if let appDelegate = UIApplication.shared.delegate as? AppDelegate {
+            appDelegate.orientationLock = orientation
+        }
+        else {
+            print("[AppUtility] error casting app delegate instance")
+        }
+    }
+    
+    static func lockOrientation(_ orientation: UIInterfaceOrientationMask, andRotateTo rotateOrientation:UIInterfaceOrientation) {
+        self.lockOrientation(orientation)
+        UIDevice.current.setValue(rotateOrientation.rawValue, forKey: "orientation")
+    }
+    
+}
