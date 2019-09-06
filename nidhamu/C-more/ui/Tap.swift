faba4af03fff9743140b878bb139244f7613f844
gladiusKatana <garthsnyder@protonmail.com>
2019-09-06 00:06:50 -0400

    Changed cell colour animations for tapping cell to add time block

--- nidhamu/C-more/ui/Tap.swift
+++ nidhamu/C-more/ui/Tap.swift
@@ -8,8 +8,8 @@ extension CollectionVC {
         let cell = collectionView.cellForItem(at: indexPath) as! CustomCell
         let layout = downcastLayout!;       let row = indexPath.item;       let column = indexPath.section
         
-        //        //print("\nselected date (unformatted gmt)  \(cell.cellDate)")
-        //        print(formattedDateString(cell.cellDate, roundedDown: false, prefix: "                 (formatted)    ", suffix: "", short: false))
+        //print("\nselected date (unformatted gmt)  \(cell.cellDate)")
+        //print(formattedDateString(cell.cellDate, roundedDown: false, prefix: "                 (formatted)    ", suffix: "", short: false))
         
         if row >= layout.lockedHeaderRows && column >= layout.lockedHeaderSections {
             selectedCellDate = cell.cellDate
@@ -19,36 +19,18 @@ extension CollectionVC {
                 
             case .hours:
                 
-                if row >= 18 {
-                    downcastLayout?.autoFitHScale = CGFloat(18 - 5 / downcastLayout!.cellHeight!) / CGFloat(layout.rows) // 5 pixel gap
-                    reloadWithDelay(after: 0)
-                }
-                
-                if !cell.markedForItems {
-                    cell.markedForItems = true
-                    
-                    if selectedTimeBlockPath == defaultPathOutOfView {
-                        UIView.animate(withDuration: 1, delay: 0,                               //* factor/put in Animations.swift
-                            usingSpringWithDamping: 1, initialSpringVelocity: 1, options: UIView.AnimationOptions.curveEaseOut, animations: {
-                                cell.backgroundColor = eventAddingColour
-                        }, completion: nil)
-                    }
-                    else {
-                        DispatchQueue.main.asyncAfter(deadline: .now()) {
+                if selectedTimeBlockPath == defaultPathOutOfView {  print("animating..")
+                    UIView.animate(withDuration: 2, delay: 0,                               //! factor/put in Animations.swift
+                        usingSpringWithDamping: 1, initialSpringVelocity: 1, options: UIView.AnimationOptions.curveLinear, animations: {
                             cell.backgroundColor = eventAddingColour
-                            self.reloadCV()
-                        }
-                    }
+                    }, completion: nil) //{ (finished:Bool) in self.populateOrOpenTimeBlock(column: column, row: row, dateString: dateString)}
+                }
+                else {
+                    cell.backgroundColor = eventAddingColour
+                    reloadCV()
                 }
                 
-                selectedTimeBlockPath = [column, row]                               //; print("selected time block path \(selectedTimeBlockPath)")
-                timeBlock = TimeBlock(values:(column, row))
-                
-                if eventsAtIndexPath[timeBlock] == nil {
-                    formatAndPresentTextField(dateString: dateString)
-                    textFieldDisplayed = true
-                }
-                else {gotoView(vc: todoListVC)}
+                populateOrOpenTimeBlock(column: column, row: row, dateString: dateString, layout: layout)
                 
             case .todoList:
                 
@@ -58,6 +40,23 @@ extension CollectionVC {
             default: print("unrecognized collection view type's cell selected")}
             
         } else {print("selected header")}
+    }
+    
+    
+    func populateOrOpenTimeBlock(column: Int, row: Int, dateString: String, layout: CustomFlowLayout) {
+        selectedTimeBlockPath = [column, row]       //; print("selected time block path \(selectedTimeBlockPath)")
+        timeBlock = TimeBlock(values:(column, row))
+        
+        if eventsAtIndexPath[timeBlock] == nil {
+            if row >= 18 {                               let gap = CGFloat(5) // extra gap for better aesthetics
+                downcastLayout?.autoFitHScale = CGFloat(18 - gap / downcastLayout!.cellHeight!) / CGFloat(layout.rows)
+                reloadWithDelay(after: 0)
+            }
+            
+            self.formatAndPresentTextField(dateString: dateString)
+            textFieldDisplayed = true
+        }
+        else {self.gotoView(vc: todoListVC)}
     }
 }
 
