74df0d352d10b2f020561eefed9ba762b17d8e43
gladiusKatana <garthsnyder@protonmail.com>
2019-09-11 14:01:19 -0400

    Eliminated extraneous time-delay upon tapping lower-down cell when keyboard presented; cleanup on Tap.swift

--- nidhamu/C-more/ui/Tap.swift
+++ nidhamu/C-more/ui/Tap.swift
@@ -4,10 +4,9 @@ import UIKit
 extension CollectionVC {
     
     override func collectionView(_ collectionView: UICollectionView,
-                                 didSelectItemAt indexPath: IndexPath) {                        //print("Tap")//print("selected tt cell")
+                                 didSelectItemAt indexPath: IndexPath) {                         //print("tapped tt cell")
         let cell = collectionView.cellForItem(at: indexPath) as! CustomCell
         let layout = downcastLayout!;       let row = indexPath.item;       let column = indexPath.section
-        
         //print("\nselected date (unformatted gmt)  \(cell.cellDate)")
         //print(formattedDateString(cell.cellDate, roundedDown: false, prefix: "                 (formatted)    ", suffix: "", short: false))
         
@@ -30,51 +29,35 @@ extension CollectionVC {
                             UIView.animate(withDuration: 2, delay: 0,                               //! factor/put in Animations.swift
                                 usingSpringWithDamping: 1, initialSpringVelocity: 1, options: UIView.AnimationOptions.curveLinear, animations: {
                                     cell.backgroundColor = eventAddingColour
-                            }, completion: nil) /**/
+                            }, completion: nil)
                         }
                     }
                     else {
                         cell.backgroundColor = eventAddingColour
                         reloadCV()
                     }
-                    
-                    presentTextFieldAndOrResizeTimetable(column: column, row: row, dateString: selectedTimeBlockDateDescription, fromAutorotate: false, layout: layout)
-                }
-                else { DispatchQueue.main.asyncAfter(deadline: .now()) {
-                    self.gotoView(vc: todoListVC)
+
+                    if row >= 18 {
+                        if !textFieldDisplayed {presentTextFieldWithBool(after: 1, dateString: selectedTimeBlockDateDescription)}
+                        else {presentTextFieldWithBool(after: 0, dateString: selectedTimeBlockDateDescription)}
                     }
-                }
+                    else {presentTextFieldWithBool(after: 0, dateString: selectedTimeBlockDateDescription)}
+                    
+                } else { DispatchQueue.main.asyncAfter(deadline: .now()) {self.gotoView(vc: todoListVC)}}
                 
             case .todoList:
                 formatAndPresentTextField(dateString: selectedTimeBlockDateDescription)
                 
             default: print("unrecognized collection view type's cell selected")}
+            
         } else {print("selected header")}
     }
     
     
-    func presentTextFieldAndOrResizeTimetable(column: Int, row: Int, dateString: String, fromAutorotate: Bool, layout: CustomFlowLayout) {
-        if row >= 18 {
-            DispatchQueue.main.asyncAfter(deadline: .now() + 1) { //[weak self] in
-                self.presentTextFieldWithBool(after: 0, dateString: dateString)
-            }
-        }
-        else {
-            presentTextFieldWithBool(after: 0, dateString: dateString)
-        }
-    }
-    
-    
     func presentTextFieldWithBool(after delay: Double, dateString: String) {
-        //if !textFieldDisplayed {
         DispatchQueue.main.asyncAfter(deadline: .now() + delay) { [weak self] in
             self?.formatAndPresentTextField(dateString: dateString)
         }
-        //} //else {print("text field already displayed")}
     }
 }
-
-/*}, completion: { (finished:Bool) in
- self?.populateOrOpenTimeBlock(column: column, row: row, dateString: dateString, layout: layout)
- })*/
 
