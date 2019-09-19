390c79b355e06db73aa9064b563386a1c8099742
gladiusKatana <garthsnyder@protonmail.com>
2019-09-18 23:17:49 -0400

    Updated feature roadmap (see Notes.swift)

--- nidhamu/Core/Notes.swift
+++ nidhamu/Core/Notes.swift
@@ -6,37 +6,41 @@
  (1st commit)                                       July. 08 2019
  
  
+ darken todo list text
+ truncate long timeblock label with  …                                                  ui
+ text field appear above keyboard
  
- mark item recurring (remove item by default)
+ mark item recurring (remove by default)                                                data
  timetableVC deferral-date selection mode
- 
- text field appear above keyboard                                                       ui
 
  eventsAtDate
  archive data (eventsAtIndexPath; eventsAtDate)
- CSV email archived data
+ archive CSV email
  Numbers template
- cascading deferrals
- ___________________________________________________Wed Sept 18                         LAUNCH BETA
+ ___________________________________________________Thu Sept 19         beta                            (ambitious)
  
  
  
+ login date interval outlined, not coloured                                             ui
  
- list VC (today items)                                                                  UX
+ eventsAtDate show on calendarVC                                                        data
+ list VC (today items)
  list VC (deadlineless items)
  morn/aft/eve/nite VC
- calendarVC; populate with eventsAtDate
- ___________________________________________________Fri Sept 20
+ settings menu
+ ___________________________________________________Fri Sept 20         appstore
  
  
  
- 
- login date interval outlined, not coloured                                             UI
- truncate long label with  …
- *direct resizing: rotate with keyboard
- settings
- darkMode
+ cascading deferrals                                                                    *
+ darkMode                                                                               design
  buttons
- launchScreen
  icon
- ___________________________________________________asap*/
+ launchScreen
+
+ direct resizing: rotate with keyboard                                                  ui
+ ___________________________________________________asap updates: appstore
+
+
+
+ * denotes features that may not necessarily be a good idea to implement. eg should users be able to defer *all* their remaining tasks at once?*/
