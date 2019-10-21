b9db9541c905116fb73df397db2e1760421588a4
gladiusKatana <garthsnyder@protonmail.com>
2019-10-20 23:59:22 -0400

    Notes.swift updated & renamed to Tasks.swift (more descriptive, since it is in fact a todo list)

---
+++ nidhamu/Core/Tasks.swift
@@ -0,0 +1,65 @@
+ /* Tasks            ･   nidhamu   ･                                deadline                        type *          comments
+  
+  
+  
+  ! handle daylight savings
+  ! defer task on multi-task block
+  
+  popup dismiss done tagging only
+  email plural
+  
+  nav buttons cleanup
+  nav buttons colouring rewrite
+  
+  
+  Numbers template                                                                                   UX
+  Excel template
+  archive frequency reminders on*
+  *settings menu                                                    Mon Oct 21
+  
+  
+  buttons                                                                                            Design
+  icon
+  launch screen
+  triple check logic, refactor, cleanup
+  run on same devices as Beta Users'
+  improve comments                                                  Oct 28 (public beta)
+  
+  
+  eventsAtDate on*                                                                                   Data
+  *calendarVC                                                                                        VCs
+  mornAftEveNiteVC
+  listVC (today items)
+  listVC (deadlineless items)                                        Nov ~15 (appstore)
+  
+  
+  login date interval outlined/animated, not perma-coloured
+  font enums for different devices
+  dark mode                                                                                          UI
+  
+  cascading deferrals?                                                                               ✧
+  loopWeeks = false behaviour as setting?                                                            ✧
+  prevent User changing nowDate via Settings app                                                     ✧
+  
+  
+  
+  
+  1st commit                                                         July 8 '19
+  
+  
+  
+  
+  
+  Notes
+  
+  
+  * DATA, UI (etc) labels map loosely to M, V, C (etc).  Tasks.swift todo list emphasizes features, design, bugfixes etc., not very code-specific
+  
+  
+  ✧ denotes features that may not necessarily be a good idea to implement (eg should User be able to defer *all* their upcoming tasks at once?)
+  
+  
+  ⚔️ All Swift files throughout this version are aesthetically optimized for a 13" screen
+  
+  
+  ✎ this is Open Source software. Always looking for contributors. https://github.com/gladiusKatana/Nidhamu */
