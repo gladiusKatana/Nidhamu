b87cb62198afe82fd33aa3212af85b001b4c9bd9
gladiusKatana <garthsnyder@protonmail.com>
2020-02-04 00:27:17 -0500

    Added file CtrCellSetup.swift

---
+++ nidhamu/C-core/CtrCellSetup.swift
@@ -0,0 +1,31 @@
+// CtrCellSetup     ･   nidhamu   ･     created by Garth Snyder   aka   gladiusKatana  ⚔️
+import UIKit
+
+extension CollectionVC {
+    
+    func centerCellSetup(cell: CenterAlignedCell, indexPath: IndexPath) -> CenterAlignedCell  {
+        
+        let customLayout = downcastLayout!
+        let row = indexPath.item;                       //let column = indexPath.section;
+        let headerRows = customLayout.lockedHeaderRows  //; let headerSections = customLayout.lockedHeaderSections
+        var fontSize = 0
+        
+        cell.backgroundColor = headerColour
+        
+        if row >= headerRows { /// time-of-day labels
+            if timeBlockSize == 1 {
+                var ampm = ""
+                if row < headerRows + 12 {ampm = "am"}
+                else {ampm = "pm"}
+                cell.titleLabel.text = "\(amPmHours[row - headerRows])\(ampm)"
+            }
+            else {cell.titleLabel.text = dayQuarters[row - headerRows]}
+            
+            if timeBlockSize == 1 && textFieldDisplayed && currentOrientation == "landscape" {fontSize = 7} else {fontSize = 11}
+            cell.titleLabel.font = UIFont.systemFont(ofSize: CGFloat(fontSize), weight: .light)
+            
+        }
+        return cell
+    }
+}
+
